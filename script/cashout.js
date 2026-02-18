632137b6016e4d317ba5f36d09db5034d82df408
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-02-18 20:33:16 +0600

    cashout functionality added

---
+++ script/cashout.js
@@ -0,0 +1,38 @@
+document.getElementById("cashout-btn")
+    .addEventListener("click", function () {
+
+        // get the agent number and validate
+        const cashoutNumberInput = document.getElementById("cashout-number");
+        const cashoutNumber = cashoutNumberInput.value;
+        if (cashoutNumber.length !== 11) {
+            alert("Invalid Agent Number");
+            return;
+        }
+
+        // get the amount
+        const cashoutAmountInput = document.getElementById("cashout-amount");
+        const cashoutAmount = cashoutAmountInput.value;
+
+        // get the current balance
+        const balanceElement = document.getElementById("balance");
+        const balance = balanceElement.innerText;
+
+        // calculate new balance
+        const newBalance = Number(balance) - Number(cashoutAmount);
+        if (newBalance < 0) {
+            alert("Invalid Amount");
+            return;
+        }
+
+        // get the pin and verify
+        const cashoutPinInput = document.getElementById("cashout-pin");
+        const pin = cashoutPinInput.value;
+        if (pin === "1234") {
+            alert("Cashout Successful");
+            balanceElement.innerText = newBalance;
+        }
+        else {
+            alert("Invalid Pin");
+            return;
+        }
+    });
